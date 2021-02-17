c1165da2f5bb8e580b430aa71b5347d8a1836343
Marina <marinella2012@gmail.com>
2021-02-17 17:20:43 +0400

    init commit

--- api/models/title.py
+++ api/models/title.py
@@ -1,5 +1,20 @@
 from django.db import models
+from category import Category
+from genre import Genre
 
 
 class Title(models.Model):
-    pass
+    name = models.TextField(max_length=50)
+    year = models.IntegerField('Год выпуска')
+    description = models.TextField(max_length=200, null=True, blank=True)
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
