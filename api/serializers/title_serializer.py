a0e6675f1fde953adbf3bcead40cc5a486819fbb
Maksim Golubev <maxgoal00@gmail.com>
2021-02-21 18:53:44 +0600

    returned title_serializer

--- api/serializers/title_serializer.py
+++ api/serializers/title_serializer.py
@@ -1,31 +1,28 @@
 from rest_framework import serializers
+from statistics import mean
+from django.shortcuts import get_object_or_404
 
-from ..models import Title, Genre, Category
+from ..models import Genre, Title
 from .category_serializer import CategorySerializer
 from .genre_serializer import GenreSerializer
 
 
-class TitleSlugSerializer(serializers.ModelSerializer):
-    genre = serializers.SlugRelatedField(
-        many=True,
-        slug_field='slug',
-        queryset=Genre.objects.all()
-    )
-    category = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Category.objects.all()
-    )
-
-    class Meta:
-        model = Title
-        fields = '__all__'
-
-
 class TitleSerializer(serializers.ModelSerializer):
-    genre = GenreSerializer(many=True)
     category = CategorySerializer()
-    rating = serializers.FloatField()
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(many=True)
 
     class Meta:
         model = Title
         fields = '__all__'
+
+    def get_rating(self, obj):
+        scores_list = obj.reviews.all().values_list('score', flat=True)
+        return mean(scores_list)
+
+    def create(self, validated_data):
+        genres_data = validated_data.pop('genre')
+        title = Title.objects.create(**validated_data)
+        for genre_data in genres_data:
+            genre = get_object_or_404(Genre, slug=genre_data)
+            title.genre_set.add(genre)
--- api/views/title_views.py
+++ api/views/title_views.py
@@ -1,18 +1,17 @@
 from rest_framework import viewsets
 
-from ..serializers.title_serializer import TitleSerializer, TitleSlugSerializer
+from ..serializers.title_serializer import TitleSerializer
 from ..models import Title
 
 
 class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
     serializer_class = TitleSerializer
     permission_classes = []
     filter_backends = []
+    #
+    # def get_serializer_class(self):
+    #     if self.action in ('create', 'partial_update'):
+    #         return TitleSlugSerializer
+    #     return TitleSerializer
 
-    def get_serializer_class(self):
-        if self.action in ('create', 'partial_update'):
-            return TitleSlugSerializer
-        return TitleSerializer
-
-    def get_queryset(self):
-        return Title.objects.annotate(rating=Avg('reviews__score'))
