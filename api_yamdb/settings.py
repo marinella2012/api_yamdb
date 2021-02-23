b3749333f57ca9b702d209e0bb51a5c25c39345d
Marina <marinella2012@gmail.com>
2021-02-23 12:47:07 +0400

    fix views, genre serializer, views

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -117,8 +117,7 @@ REST_FRAMEWORK = {
         'PAGE_SIZE': 20,
     }
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
