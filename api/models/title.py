9cf3659da6d137d7df6920d3de95c85c02502309
Marina <marinella2012@gmail.com>
2021-02-27 14:32:03 +0400

    fix for the 2 review

--- api/models/title.py
+++ api/models/title.py
@@ -1,7 +1,16 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from .category import Category
 from .genre import Genre
+
+
+def no_future(value):
+    today = int(date.today().strftime('%Y'))
+    if value > today:
+        raise ValidationError('Year cannot be in the future.')
 
 
 class Title(models.Model):
@@ -11,7 +20,8 @@ class Title(models.Model):
     )
     year = models.PositiveIntegerField('Год выпуска',
                                        null=True,
-                                       blank=True)
+                                       blank=True,
+                                       validators=[no_future])
     description = models.TextField(max_length=200, null=True, blank=True)
     category = models.ForeignKey(
         Category,
